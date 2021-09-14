67a425c22744c3af6a87ec4d7497c414b04dbecd
jsun <jaysun054@gmail.com>
2021-09-13 20:33:26 -0700

    recent searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var inputEl = document.querySelector("#input");
 var resultsEl = document.querySelector("#results");
 var modalEl = document.querySelector("#modal");
 var closeEl = document.querySelector("#close");
+var clearEl = document.querySelector("#clear");
+var storageEl = [];
 
 //handle input
 var inputHandler = function(event) {
@@ -11,7 +13,7 @@ var inputHandler = function(event) {
     if (inputVal) {
         api(inputVal);
         inputEl.value = "";
-        
+	
     } else {modalEl.classList.add("is-active");}
 };
 
@@ -34,6 +36,11 @@ var api = function(val) {
 					if (document.querySelector("#resList")) {
 						document.querySelector("#resList").remove();
 					}
+					if (document.querySelector("#reList")) {
+						document.querySelector("#reList").remove();
+					}
+					save(val);
+					load();
 					displayRes(data);
 				} else {modalEl.classList.add("is-active");}
 			})
@@ -66,5 +73,56 @@ var modalHandler = function(event) {
 	}
 }
 
+//save function
+var save = function(song) {
+    //prevent dulpicate saves
+    for (var i = 0; i < storageEl.length; i++) {
+        if (song === storageEl[i]) {
+            //splice
+            storageEl.splice(i, 1);
+        }
+    }
+    //push and set to local storage
+    storageEl.push(song);
+    localStorage.setItem("songs", JSON.stringify(storageEl));
+	console.log(localStorage);
+};
+
+//load function
+var load = function() {
+    storageEl = JSON.parse(localStorage.getItem("songs")) || [];
+    var recent = document.querySelector("#recent");
+    var reList = document.createElement("div");
+    reList.setAttribute("id", "reList");
+    recent.append(reList);
+
+    //loop
+    for (var i = 0; i < storageEl.length; i++) {
+        var reItem = document.createElement("button");
+        reItem.setAttribute("type", "button");
+        reItem.setAttribute("name", storageEl[i]);
+        reItem.innerHTML = storageEl[i];
+        reItem.className = "button is-white";
+        //prepend
+        reList.prepend(reItem);
+    }
+	var reClick = document.querySelector("#reList");
+    reClick.addEventListener("click", searchRe);
+};
+
+//search recent
+var searchRe = function(event) {
+    var clicked = event.target.getAttribute("name");
+    api(clicked);
+};
+
+//clear function
+var clear = function(event) {
+    localStorage.clear();
+    location.reload();
+};
+
+load();
 userFormEl.addEventListener("submit", inputHandler);
 closeEl.addEventListener("click", modalHandler);
+clearEl.addEventListener("click", clear);
