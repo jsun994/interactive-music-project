8202767e49bcd0526a4390925c29f641bf30ad35
cgar246 <christiang3268@yahoo.com>
2021-09-14 19:38:58 -0700

    adding genuis api

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,24 @@ var songGone = function(event){
     }
 }
 
-var songSearch = function(recentSearch) {
-    var songApiUrl = "https://api.mixcloud.com/search/?=" + recentSearch & type
+var songSearch = function(axios) {
+    var axios = require("axios").default;
+    console.log(axios)
+var options = {
+    method: 'GET',
+    url: 'https://genius.p.rapidapi.com/search',
+    params: {q: ''},
+    headers: {
+    'x-rapidapi-host': 'genius.p.rapidapi.com',
+    'x-rapidapi-key': 'b0955a5cf0msh85a6f596e0511d6p1965f0jsn260a9f2c67e0'
+}
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+}).catch(function (error) {
+	console.error(error);
+});
 }
 clearSongEl.addEventListener("click", songGone)
+searchNowEl.addEventListener("click", songSearch)
