67104c5bbab0d3ddf16057d0c1c981e11e52ee37
cgar246 <christiang3268@yahoo.com>
2021-09-14 20:16:01 -0700

    added new Html to search for top artist hits

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,39 @@
-var clearSongEl = document.querySelector("#all-clear")
-var searchNowEl = document.querySelector("#searchnow")
+var userFormEl = document.querySelector("#form");
+var inputEl = document.querySelector("#input");
+var resultsEl = document.querySelector("#results");
+var modalEl = document.querySelector("#modal");
+var closeEl = document.querySelector("#close");
+var clearEl = document.querySelector("#clear");
 
-var songGone = function(event){
+var storageEl = [];
+
+//handle input
+var inputHandler = function(event) {
     event.preventDefault();
-    console.log(event);
-    console.log(searchNowEl)
-    var recentSearch = searchNowEl.value.trim()
-    console.log(recentSearch)
-    if(recentSearch){
-        songSearch(recentSearch)
-        searchNowEl.value=""
-    }else {
-        alert("Are you sure you'd like to clear Recent Searches")
-    }
-}
+    var inputVal = inputEl.value.trim();
+    if (inputVal) {
+        api(inputVal);
+        inputEl.value = "";
+	
+    } else {modalEl.classList.add("is-active");}
+};
 
-
+//api
 var api = function(val) {
-	fetch("https://genius.p.rapidapi.com/search?q=", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "genius.p.rapidapi.com",
-		"x-rapidapi-key": "b0955a5cf0msh85a6f596e0511d6p1965f0jsn260a9f2c67e0"
-	}
-    })
+	fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=" + val, {
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+			"x-rapidapi-key": "2eba6a7cffmsh74d3bc58b3229c1p11d82fjsn2bc3b902cbfc"
+		}
+	})
 	.then(function(response) {
 		//console.log(response);
 		if (response.ok) {
     		response.json().then(function(apiData) {
 				console.log(apiData);
 			
-				if (apiData.data.length!= 0) {
+				if (apiData.data.length != 0) {
 					if (document.querySelector("#resList")) {
 						document.querySelector("#resList").remove();
 					}
@@ -45,6 +48,8 @@ var api = function(val) {
 		} else {modalEl.classList.add("is-active");}
 	})
 };
+
+//display
 var displayRes = function(apiData) {
 	
 	var list = document.createElement("div");
@@ -60,5 +65,65 @@ var displayRes = function(apiData) {
 		list.append(resItem);
 	}
 };
-clearSongEl.addEventListener("click", songGone)
-searchNowEl.addEventListener("click", api)
+
+//modal
+var modalHandler = function(event) {
+	modalEl.classList.remove("is-active");
+	if (document.querySelector("#resList")) {
+		document.querySelector("#resList").remove();
+	}
+}
+
+//save function
+var save = function(song) {
+    //prevent dulpicate saves
+    for (var i = 0; i < storageEl.length; i++) {
+        if (song === storageEl[i]) {
+            //splice
+            storageEl.splice(i, 1);
+        }
+    }
+    //push and set to local storage
+    storageEl.push(song);
+    localStorage.setItem("songs", JSON.stringify(storageEl));
+	console.log(localStorage);
+};
+
+//load function
+var load = function() {
+    storageEl = JSON.parse(localStorage.getItem("songs")) || [];
+    var recentEl = document.querySelector("#recent");
+    var reList = document.createElement("div");
+    reList.setAttribute("id", "reList");
+    recentEl.append(reList);
+
+    //loop
+    for (var i = 0; i < storageEl.length; i++) {
+        var reItem = document.createElement("button");
+        reItem.setAttribute("type", "button");
+        reItem.setAttribute("name", storageEl[i]);
+        reItem.innerHTML = storageEl[i];
+        reItem.className = "button is-white";
+        //prepend
+        reList.prepend(reItem);
+    }
+	var reClick = document.querySelector("#reList");
+    reClick.addEventListener("click", searchRe);
+};
+
+//search recent
+var searchRe = function(event) {
+    var clicked = event.target.getAttribute("name");
+    api(clicked);
+};
+
+//clear function
+var clear = function(event) {
+    localStorage.clear();
+    location.reload();
+};
+
+load();
+userFormEl.addEventListener("submit", inputHandler);
+closeEl.addEventListener("click", modalHandler);
+clearEl.addEventListener("click", clear);
