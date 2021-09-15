d9ecfa7dc0643a49495570fcc0a93e28d202c5a9
cgar246 <christiang3268@yahoo.com>
2021-09-14 19:47:47 -0700

    adding changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,24 +15,35 @@ var songGone = function(event){
     }
 }
 
-var songSearch = function(axios) {
-    var axios = require("axios").default;
-    console.log(axios)
-var options = {
-    method: 'GET',
-    url: 'https://genius.p.rapidapi.com/search',
-    params: {q: ''},
-    headers: {
-    'x-rapidapi-host': 'genius.p.rapidapi.com',
-    'x-rapidapi-key': 'b0955a5cf0msh85a6f596e0511d6p1965f0jsn260a9f2c67e0'
-}
-};
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
-}
+var api = function(val) {
+	fetch("https://genius.p.rapidapi.com/search?q=", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "genius.p.rapidapi.com",
+		"x-rapidapi-key": "b0955a5cf0msh85a6f596e0511d6p1965f0jsn260a9f2c67e0"
+	}
+    })
+	.then(function(response) {
+		//console.log(response);
+		if (response.ok) {
+    		response.json().then(function(apiData) {
+				console.log(apiData);
+			
+				if (apiData.data.length != 0) {
+					if (document.querySelector("#resList")) {
+						document.querySelector("#resList").remove();
+					}
+					if (document.querySelector("#reList")) {
+						document.querySelector("#reList").remove();
+					}
+					save(val);
+					load();
+					displayRes(apiData);
+				} else {modalEl.classList.add("is-active");}
+			})
+		} else {modalEl.classList.add("is-active");}
+	})
+};
 clearSongEl.addEventListener("click", songGone)
-searchNowEl.addEventListener("click", songSearch)
+searchNowEl.addEventListener("click", api)
